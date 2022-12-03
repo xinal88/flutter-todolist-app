baad5e4c63ba781def956e7a2db80588b1bfc058
xinal88 <dangquy.bui88@gmail.com>
2022-12-03 22:37:40 +0700

    Flutter ToDoList App

---
+++ lib/model/todo.dart
@@ -0,0 +1,34 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Learn Calculus 1', isDone: true),
+      ToDo(id: '02', todoText: 'Learn Algebra', isDone: true),
+      ToDo(
+        id: '03',
+        todoText: 'Check Emails',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'GDSC Meeting',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'Learn mobile apps',
+      ),
+      ToDo(
+        id: '06',
+        todoText: 'Do Calculus test',
+      ),
+    ];
+  }
+}
